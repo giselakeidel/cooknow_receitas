fcae900bccc9def30ad80bf5b1a22076aeae8cf3
Gisela Keidel <giselakeidel@gmail.com>
2022-06-12 18:20:50 -0300

    Adicionando Campo Modo de Preparo no Schema e Mudando a Index

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_12_202638) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_12_210211) do
   create_table "recipes", force: :cascade do |t|
     t.string "name"
     t.text "stuff"
     t.integer "calories"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "prepare_mode"
   end
 
 end
