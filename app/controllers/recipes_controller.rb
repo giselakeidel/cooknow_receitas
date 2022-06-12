e3a5ceddaa7d260ecb7453d48f53dfba81732d5c
Gisela Keidel <giselakeidel@gmail.com>
2022-06-12 19:16:29 -0300

    Criando Form de Edição

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,4 +5,13 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+  def update
+    @recipe = Recipe.find(params[:id])
+    recipe_params = params.require(:recipe).permit(:name, :stuff, :calories, :prepare_mode)
+    @recipe.update(recipe_params)
+    redirect_to @recipe
+  end
 end
