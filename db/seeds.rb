be070859726dedf244788a1342498d69feb1c391
Gisela Keidel <giselakeidel@gmail.com>
2022-06-15 11:26:08 -0300

    inserindo novas receitas

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,6 +38,66 @@ Recipe.create!([
     portion: "4",
     duration: "00:10:00",
     poster: "salada_peito_peru.jpeg"
-  }
+  },
+  {
+    name: "Berinjela recheada low carb",
+    stuff: "1 berinjela grande, 150 gramas de carne moída (sugestão: patinho bem limpo), 1 cebola pequena picada, 1 tomate picado, 1 colher de sopa de molho de tomate, Salsinha, sal e pimenta a 	 gosto, 1 colher de sobremesa de requeijão light (opcional)",
+    calories: 95,
+    kind: "Saladas",
+    prepare_mode: "Reúna todos os ingredientes; Higienize a berinjela, corte ela no meio e retire o miolo com a ajuda de uma colher; Pique o miolo e reserve; Em uma panela, adicione a carne moída, a cebola e refogue bem; Acrescente o miolo da berinjela picado, o tomate, o molho, a salsinha, o sal, a pimenta, misture bem e deixe cozinhar um pouco; Recheie as berinjelas com a carne moída e passe por cima, requeijão light; Disponha elas em uma forma, com um dedinho de água e leve para assar em forno preaquecido a 180 ºC por cerca de 20 minutos; Agora é só servir.",
+    portion: "2",
+    duration: "00:50:00",
+    poster: "berinjela-recheada-low-carb.jpg"
+  },
+  {
+    name: "Barrinhas de cereal",
+    stuff: "1 xícara de nozes, 1 xícara de frutas secas, 1 xícara de tâmaras secas sem caroço",
+    calories: 75,
+    kind: "Petisco",
+    prepare_mode: "Bata todos os ingredientes em um processador por cerca de dois minutos, até que formem uma massa. Embrulhe a mistura em um papel filme e leve à geladeira por algumas horas até sentir que a massa está dura. Corte em formato de barras e conserve na geladeira.",
+    portion: "1",
+    duration: "00:15:00",
+    poster: "barrinha_cereal.jpeg"
+  },
+  {
+    name: "Mousse de limão",
+    stuff: "1 lata de leite condensado, 1 lata de creme de leite, 1/2 xícara de limão espremido",
+    calories: 230,
+    kind: "Sobremesa",
+    prepare_mode: "Coloque o creme de leite e o leite condensado no liquidificador, bata um pouco e acrescente o suco de limão aos poucos. Quando a massa ficar consistente, coloque em um recipiente de vidro e leva à geladeira.",
+    portion: "6",
+    duration: "00:40:00",
+    poster: "mousse_limao.jpeg"
+    },
+    {
+      name: "Sorvete Caseiro",
+      stuff: "1 lata de leite condensado, 1 lata de creme de leite, 1 pacote de gelatina do seu sabor preferido",
+      calories: 450,
+      kind: "Sobremesa",
+      prepare_mode: "Prepare a gelatina normalmente (sem levar à geladeira) e enquanto ela ainda estiver quente bata no liquidificador junto com o creme de leite e o leite condensado. Coloque para congelar em um refratário com tampa, bata na batedeira (enquanto estiver congelado) e leve ao freezer novamente.",
+      portion: "8",
+      duration: "01:30:00",
+      poster: "sorvete_caseiro.jpeg"
+      },
+      {
+        name: "Pão de Queijo",
+        stuff: "1 caixa de creme de leite, 1 copo de queijo parmesão ralado, 1 copo de polvilho doce",
+        calories: 180,
+        kind: "Lanche",
+        prepare_mode: "Pré aqueça o forno enquanto amassa todos os ingredientes até eles formarem uma massa (que não grude nas mãos), faça bolinhas e coloque para assar em uma forma untada.",
+        portion: "10",
+        duration: "00:25:00",
+        poster: "pao_queijo.jpeg"
+        },
+        {
+          name: "Bolo para café da tarde",
+          stuff: "3 ovos, 2/3 de xícara de farinha com fermento, 1/2 xícara de açúcar refinado",
+          calories: 230,
+          kind: "Bolo",
+          prepare_mode: "Pré aqueça o forno a 190ºC. Em uma tigela, bata os ovos e o açúcar até que formem um creme, depois misture a farinha. Coloque a massa em uma forma untada e asse.",
+          portion: "12",
+          duration: "00:45:00",
+          poster: "bolo.jpeg"
+          }
 
 ])
